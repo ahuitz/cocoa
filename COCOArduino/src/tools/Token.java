ba54f33a59cc57252a02a05234f5a13fe0b7f528
Rosario <Rosario@AranolyH>
2014-10-19 12:27:48 -0600

    Primera parte, solo analizador lexico

---
+++ COCOArduino/src/tools/Token.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tools;
+
+/**
+ *
+ * @author Rosario
+ */
+public class Token {
+ public String identificador;
+ public String cadena;
+}
